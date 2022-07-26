d4f3f881cf1950c6ba05b2b9fa35f8e0804d2cbc
sofynacr0112 <sofynacintarahmawati01@gmail.com>
2022-07-26 11:57:04 +0700

    soal project

---
+++ latihan.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div class="mb-3">
+                                <label class="form-label">Angka</label>
+                                <input type="number" class="form-control  @error('angka') is-invalid @enderror"
+                                    name="angka">
+                                 </div> <br>
+                                 <div class="mb-3">
+                                 
+                                <div class="d-grid gap-2">
+                                    <button class="btn btn-primary" type="submit">Pilih</button>
+                                </div>
+                                
+
+</body>
+</html>
---
+++ latihan2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div class="mb-3">
+                                <label class="form-label">Pilih</label>
+                                <input type="text" class="form-control  @error('pilih') is-invalid @enderror"
+                                    name="pilih">
+                                 </div> <br>
+                                 <div class="mb-3">
+                                 
+                                <div class="d-grid gap-2">
+                                    <button class="btn btn-primary" type="submit">Pilih</button>
+                                </div>
+</body>
+</html>
